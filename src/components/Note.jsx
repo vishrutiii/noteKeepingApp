a4047efbfd6033e302fcb69079bbd880a5a17832
Vishruti Chauhan <cvishruti01@gmail.com>
2021-10-28 20:07:42 +0530

    first

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1> this is the title </h1>
+            <p> this is the contents </p>
+        </div>
+    );
+}
+
+export default Note;
